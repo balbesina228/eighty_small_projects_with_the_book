3b5795b47922f3f191341a92bf8e40091b164a8d
Ilya Ponomarev <109335756+balbesina228@users.noreply.github.com>
2022-11-11 23:54:34 +0700

    Create factor_finder.py

---
+++ factor_finder.py
@@ -0,0 +1,34 @@
+from sys import exit
+
+print('''
+Factor Finder, by Al Sweigart. (made a little modification by myself)
+
+A number's factors are two numbers that, when multiplied with each
+other, produce the number. For example, 2 x 13 = 26, so 2 and 13 are
+factors of 26. 1 x 26 = 26, so 1 and 26 are also factors of 26. We
+say that 26 has four factors: 1, 2, 13, and 26.
+
+If a number only has two factors (1 and itself), we call that a prime
+number. Otherwise, we call it a composite number.
+Can you discover some prime numbers?
+''')
+
+while True:
+    response = input('Enter a number or "QUIT" to quit.\n> ').upper()
+    if response == 'QUIT':
+        exit()
+    if not (response.isdecimal() and int(response) > 0):
+        print('Wrong format.')
+        continue
+    number = int(response)
+    factors = []
+    for i in range(1, int(number ** (1 / 2) + 1)):
+        if number % i == 0:
+            factors.append(i)
+            factors.append(number // i)
+    factors = sorted(list(set(factors)))
+    for i in range(len(factors)):
+        factors[i] = str(factors[i])
+
+    print(f"{', '.join(factors)} are factors of {number}.")
+    continue
